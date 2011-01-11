d87fa66110c8c069207daf7b95e443af5655ec7f
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2011-01-11 14:45:26 -0500

    Added the second source code file

---
+++ Chapter_01/Welcome1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	// Display Welcome to C++ to the console
+	std::cout << "Welcome to C++!" << std::endl;
+	std::cout << "Welcome to C++.NET!" << std::endl;
+	std::cout << "Welcome to C++ Compiler!" << std::endl;
+	
+	return 0;
+}
